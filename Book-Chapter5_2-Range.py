486d0a21daa8c267959a69c0f38f5f1e297f94c1
Atanas <55555430+ultraasi-atanas@users.noreply.github.com>
2020-12-29 10:47:45 +0000

    Adding List Comprehension and Type Oddities files
    Adding two new files with code
    
    Touched the Range file.

--- Book-Chapter5_2-Range.py
+++ Book-Chapter5_2-Range.py
@@ -23,8 +23,57 @@ If two arguments are supplied, step is 1
 If one argument is supplied, step is 1, stop is taken from arg, start is 0 
 
 All of the operations on tuples apply on ranges, except concatenation and repetition
-"""
 
+The numbers of the progression are generated on an "as needed" basis, so even
+expressions such as range(10000) consume little memory (Python3.x) 
+
+The arguments of the range function are evaluated just before the first iteration
+of the loop and not reevaluated for subsequent iteration
+
+"""
+# RANGE SLICE
 rangeSlice = range(10)[2:6][2] # returns 4
 
-print(rangeSlice)
+print('RangeSlice of range(10)[2:6][2] is ', rangeSlice, '\n')
+
+# RANGE COMPARISON
+range2 = range(0,7,2)
+range3 = range(0,8,2)
+
+print('Range 2 is equal to Range 3?', range2 == range3)
+print('Range 2 is', range2)
+print('Range 3 is', range3)
+
+print("Range2: ")
+for e in range2: 
+      print(e)
+      
+print("Range3:")
+for e in range3:
+    print(e)
+      
+      
+# NEGATIVE STEP
+range4 = range(40,-10,-10)
+print('\n', 'Range4 is', range4)
+for i in range4:
+    print('\n', i)
+    
+    
+# EXECUTING RANGE INSIDE A FOR LOOP
+x = 5
+for i in range(0,x):
+    print(i)
+    x = 8 # it doesnt change the range set in the loop condition
+
+
+
+x = 5
+y = range(0,x)
+for i in y:
+    print('\n', y)
+    print(i)
+    x = 8 # it doesnt change the range set in the loop condition
+
+
+
---
+++ Book-Chapter5_3-List-comprehension.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 28 20:57 2020
+Introduction to Computation and Programming Using Python. John V. Guttag, 2016, 2nd ed
+Book Chapter 5 Structured types, mutability and higher-order functions
+
+List Comprehension
+
+Provides a conscise way to apply an operation to the values in a sequence. 
+It creates a new list. 
+
+A word of warning:
+"Some Python programmers use list comprehension in marvelous and subtle ways.
+That is not always a great idea. Remember that somebody else may need to 
+read your code, and 'subtle' is not usually a desired property"
+
+@author: Atanas Kozarev - github.com/ultraasi-atanas
+
+"""
+
+L = [x**2 for x in range(10)]
+
+L2 = [x**2 for x in range(10) if x%2 == 0]
+
+L3 = []
+for x in range(1000):
+    if x%2 == 0:
+        L3.append(x**2)
+
+print(L3[:10])
+
+mixed = [1,2,3,'a','b','c',5.0]
+print([e**2 for e in mixed if type(e)== int])
