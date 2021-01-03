dae5bff821550c200f158f68ddf898f495e7c5f0
Atanas <55555430+ultraasi-atanas@users.noreply.github.com>
2021-01-03 13:48:09 +0000

    Create Book-Chapter7_2-RaiseExceptions.py

---
+++ Book-Chapter7_2-RaiseExceptions.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 03 2021 - 13:36
+Introduction to Computation and Programming Using Python. John V. Guttag, 2016, 2nd ed
+Book Chapter 7 Exceptions and Assertions 
+
+Exceptions as a Control Flow Mechanism
+
+def findAnEven(L):
+    '''Assumes L is a list of integers
+        Returns the first even number in L
+        Raises ValueError if L does not contain an even number
+    '''
+    
+
+@author: Atanas Kozarev - github.com/ultraasi-atanas
+"""
+
+def findAnEven(L):
+    '''Assumes L is a list of integers
+        Returns the first even number in L
+        Raises ValueError if L does not contain an even number'''
+    
+    for elem in L:
+        if elem%2 == 0:
+            return elem
+        
+    raise ValueError("L does not contain an even number")
+   
+# =============================================================================
+# # Internal exception handling but we assume that's not needed, it will be handled
+# # at the level of which the function is being called
+#     try:
+#         for elem in L:
+#             if elem%2 == 0:
+#                 return elem
+#         raise ValueError("L does not contain an even number")
+#     except:
+#         print("Now")
+# =============================================================================
+
+print(findAnEven([1,3,5]))
